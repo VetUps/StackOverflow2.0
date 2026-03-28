49f5b8049533195666d2dcc668c1c6e2edaf3253
VetUps <sercherufa@gmail.com>
2026-03-28 23:07:40 +0500

    Ограничение на 1 решение от конкретного пользователя

--- src/backend/apps/qa/models.py
+++ src/backend/apps/qa/models.py
@@ -10,31 +10,48 @@ class Question(models.Model):
         CLOSED_STATUS = 'closed', 'Closed'
         SOLVED_STATUS = 'solved', 'Solved'
 
-    question_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=False,
-                                   help_text='Уникальный идентификатор вопроса')
-    user = models.ForeignKey(CustomUser, blank=False, null=True, on_delete=models.SET_NULL,
-                             help_text='Автор вопроса')
-    question_title = models.CharField(max_length=300, blank=False,
-                                      help_text='Краткое описание вопроса')
-    question_body = models.TextField(blank=False,
-                                     help_text='Тело вопроса (сам текст)')
-    question_status = models.CharField(choices=Status.choices, default=Status.OPEN_STATUS, blank=False,
-                                       help_text='Статус вопроса')
+    question_id =         models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=False,
+                                           help_text='Уникальный идентификатор вопроса')
+    user =                models.ForeignKey(CustomUser, blank=False, null=True, on_delete=models.SET_NULL,
+                                            help_text='Автор вопроса')
+    question_title =      models.CharField(max_length=300, blank=False,
+                                           help_text='Краткое описание вопроса')
+    question_body =       models.TextField(blank=False,
+                                           help_text='Тело вопроса (сам текст)')
+    question_status =     models.CharField(choices=Status.choices, default=Status.OPEN_STATUS, blank=False,
+                                           help_text='Статус вопроса')
     question_created_at = models.DateTimeField(auto_now_add=True, blank=False,
                                                help_text='Дата создания вопроса')
     question_updated_at = models.DateTimeField(auto_now=True, blank=False,
                                                help_text='Дата изменения вопроса')
 
+    class Meta:
+        db_table = 'questions'
+
+    def __str__(self):
+        return self.question_title
+
 class Solution(models.Model):
-    solution_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=False,
-                                   help_text='Уникальный идентификатор решения')
-    user = models.ForeignKey(CustomUser, blank=False, null=True, on_delete=models.SET_NULL,
-                             help_text='Автор решения')
-    question = models.ForeignKey(Question, blank=False, on_delete=models.CASCADE,
-                                 help_text='Вопрос к которому представлено решение')
-    solution_is_best = models.BooleanField(default=False, blank=False,
-                                           help_text='Является ли решение лучшим')
+    solution_id =         models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, blank=False,
+                                           help_text='Уникальный идентификатор решения')
+    user =                models.ForeignKey(CustomUser, blank=False, null=True, on_delete=models.SET_NULL,
+                                            help_text='Автор решения')
+    question =            models.ForeignKey(Question, blank=False, on_delete=models.CASCADE,
+                                            help_text='Вопрос к которому представлено решение')
+    solution_body =       models.TextField(blank=False,
+                                           help_text='Тело решения (сам текст)')
+    solution_is_best =    models.BooleanField(default=False, blank=False,
+                                              help_text='Является ли решение лучшим')
     solution_created_at = models.DateTimeField(auto_now_add=True, blank=False,
                                                help_text='Дата создания решения')
     solution_updated_at = models.DateTimeField(auto_now=True, blank=False,
                                                help_text='Дата изменения решения')
+
+    class Meta:
+        db_table = 'solutions'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'question'], name='unique_solution_user_pair')
+        ]
+
+    def __str__(self):
+        return self.solution_id
